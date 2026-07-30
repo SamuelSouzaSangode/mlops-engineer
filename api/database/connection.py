e3458a9b8f78a955b52f548f2050ca8482a01ad8
Samuel <pcleumas1218@gmail.com>
2026-07-30 09:40:59 -0300

    Fazendo as dependencies

--- api/database/connection.py
+++ api/database/connection.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
-
+#Conectando ao banco de dados
 DATABASE_URL = (
     "postgresql://sam:123456@postgres:5432/mlops"
 )
-
+#Criando a engine
 engine = create_engine(
     DATABASE_URL,
     echo=True
