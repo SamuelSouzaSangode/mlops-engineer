7d01507ae27745ee13e91ff1625e2ae555b64195
Samuel <pcleumas1218@gmail.com>
2026-08-02 12:14:43 -0300

    Api funcionando com modelo pkl implantado código 200

--- api/database/repositories/prediction_service.py
+++ api/database/repositories/prediction_service.py
@@ -5,13 +5,31 @@ from api.database.models import Previsao
 #Modelo fazendo previsão
 from api.services.predict import predict
 
+from pathlib import Path
+import pandas as pd
+import joblib
+
 class PredictionService:
     def __init__(self):
         #Parte de salvamento do banco e etc
         self.repository = PredictionRepository()
+        BASE_DIR = Path(__file__).resolve().parents[3]
+        MODEL_PATH = BASE_DIR / "modelos" / "modelo.pkl"
+
+        self.modelo_valor_casas = joblib.load(MODEL_PATH)
+
 
     def predict(self, db, imovel):
-        preco = predict(imovel) #Vai prever o preço
+        dados = pd.DataFrame(
+            {
+                'area': [imovel.area],
+                'quartos': [imovel.quartos],
+                'banheiros': [imovel.banheiros],
+                'garagem': [imovel.garagem]
+            }
+        )
+
+        preco = float(self.modelo_valor_casas.predict(dados)[0])
 
         #Salvando a previsão na tabela do banco de dados
         previsao = Previsao(
