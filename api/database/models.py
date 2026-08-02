42cd71b64a2ca07b7da506b63eff5c9d1ecb6b7f
Samuel <pcleumas1218@gmail.com>
2026-08-02 09:11:34 -0300

    Commit do alembic coluna com horário

--- api/database/models.py
+++ api/database/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Float
 from sqlalchemy import Integer
+from sqlalchemy import String
 
 
 from sqlalchemy.orm import DeclarativeBase
@@ -19,8 +20,10 @@ class Previsao(Base):
         Integer,
         primary_key=True
     )
+    versao: Mapped[str] = mapped_column(String(20), nullable=True)
     area: Mapped[float] = mapped_column(Float)
     quartos: Mapped[int] = mapped_column(Integer)
     banheiros: Mapped[int] = mapped_column(Integer)
     garagem: Mapped[int] = mapped_column(Integer)
     preco: Mapped[float] = mapped_column(Float)
+    horario: Mapped[str] = mapped_column(String(20), nullable=True)
